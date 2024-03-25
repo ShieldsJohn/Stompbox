d35f7071c7cf7064066067bb285d4e18170b20b2
John Shields <shields101@hotmail.com>
2024-03-25 19:27:43 +0000

    Initial deployment set up

--- stompbox/settings.py
+++ stompbox/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ls@yjq+#t18wa&*w4b6f=v3a!k**4rok=lw#y0!7^2=21i^wp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
