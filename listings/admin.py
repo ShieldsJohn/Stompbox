c5acfe7cbc8a07da0b56eb4b34829ffe177f4f25
John Shields <shields101@hotmail.com>
2024-04-06 16:01:29 +0000

    Add initial models for pedal, manufacturer and listing

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Pedal, Manufacturer, Listing
 
 # Register your models here.
 admin.site.register(Category)
-
+admin.site.register(Pedal)
+admin.site.register(Manufacturer)
+admin.site.register(Listing)
--- listings/models.py
+++ listings/models.py
@@ -6,3 +6,26 @@ class Category(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
+
+class Manufacturer(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)   
+
+class Pedal(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    manufacturer_name = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    catergory_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+
+class Listing(models.Model):
+    pedal_name = models.ForeignKey(Pedal, on_delete=models.CASCADE)
+    manufacturer_name = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    catergory_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # seller_name = models.ForeignKey(MyAccount, on_delete=models.CASCADE) #MyAccount not yet created
+    listing_date = models.DateTimeField(auto_now_add=True)
+    price = models.IntegerField()
+    description = models.TextField()
+    condition = models.CharField(max_length=200, unique=True)
+   #add listing images here 
+
