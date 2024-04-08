0b21d0651162115e064b5d3b4dd1adfa1428eda1
John Shields <shields101@hotmail.com>
2024-04-08 18:48:34 +0000

    Set up initial template structure

--- listings/urls.py
+++ listings/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='home'),
+    path('', views.base, name='base'),
+    path('', views.home, name='home'),
+    path('', views.myaccount, name='myaccount'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -4,6 +4,15 @@ from .models import Category
 
 
 # Create your views here.
-class Index(generic.ListView):
-    model = Category
-    template_name = "listings/index.html"
+# class Base(generic.ListView):
+   # model = Category
+   # template_name = "listings/base.html"
+
+def base(request):
+    return render(request, "listings/base.html")
+
+def home(request):
+    return render(request, "listings/home.html")
+
+def myaccount(request):
+    return render(request, "listings/myaccount.html")
