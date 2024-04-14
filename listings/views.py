ff4ee23c62ede8c3b004d42fe5b2bf5c7ade00ec
John Shields <shields101@hotmail.com>
2024-04-14 14:02:52 +0000

    Rename My Account to MyStompbox and make visible only to logged in users

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import generic
 from .models import Category
 
+@login_required
 def myaccount(request):
     return render(request, "listings/myaccount.html")
