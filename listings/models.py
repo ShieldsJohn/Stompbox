a3bc5fe265cabd4938b57c32432374401b5767f7
John Shields <shields101@hotmail.com>
2024-04-21 14:21:33 +0000

    Add 'title' and 'condition' dropdown to listing model

--- listings/models.py
+++ listings/models.py
@@ -7,10 +7,14 @@ class Category(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
+    def __str__(self):
+        return f"{self.title}"
 
 class Manufacturer(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)   
+    def __str__(self):
+        return f"{self.title}"
 
 class Pedal(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -18,15 +22,29 @@ class Pedal(models.Model):
     manufacturer_name = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     catergory_name = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField()
+    def __str__(self):
+        return f"{self.title}"
 
 class Listing(models.Model):
+    title = models.CharField(max_length=200, unique=True)
     pedal_name = models.ForeignKey(Pedal, on_delete=models.CASCADE)
     manufacturer_name = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    catergory_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category_name = models.ForeignKey(Category, on_delete=models.CASCADE)
     # seller_name = models.ForeignKey(MyAccount, on_delete=models.CASCADE) #MyAccount not yet created
     listing_date = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
-    condition = models.CharField(max_length=200, unique=True)
+    CONDITION_CHOICES = [
+        ('As_New', 'As New'),
+        ('Good', 'Good'),
+        ('Well_Used', 'Well Used'),
+        ('Broken', 'Broken'),
+    ]
+    condition = models.CharField(max_length=20, choices=CONDITION_CHOICES, default='Good')
    #add listing images here ref Cloudinary lesson
+    class Meta:
+        ordering = ["-listing_date"]
+    def __str__(self):
+        return f"{self.title}"
+
 
--- stompbox/settings.py
+++ stompbox/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'yourstompbox.app@gmail.com'
-EMAIL_HOST_PASSWORD = 'conzldhjkvthryfv'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'Stompbox <noreply.yourstompbox.app@gmail.com>'
 
 # Internationalization
